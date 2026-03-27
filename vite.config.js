d3b669c18676051e8500fdd5e14860d25463692d
vistaranexports <vistaranexports@gmail.com>
2026-03-27 13:30:57 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/vistaran_exports2/',
+  base: '/vistaranexports/',
   plugins: [react()],
 })
